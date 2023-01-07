a41e0f19c3c72f938f344d226b39d1de7a9c7b7d
Chaitu Vinnu <Chaitu Vinnu@CHAITUVINI>
2023-01-07 14:33:53 +0530

    Flipkart FirstCommit

---
+++ Filpkart/src/main/java/DemoFlipkart/FlipkartDemoProject.java
@@ -0,0 +1,18 @@
+package DemoFlipkart;
+
+public class FlipkartDemoProject {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("welecome to git hub");
+		
+		System.out.println("hello");
+		
+		
+		
+	}
+	
+	
+	
+
+}
